9627eb4841c1ef54b36adc4ff29b830170efe908
Fatih GÜRDAL <fatican92@gmail.com>
2017-11-29 01:06:48 +0300

    Mail gönderme ufak düzenlemeler

--- CandyFramework.Common/Mails/Mail.cs
+++ CandyFramework.Common/Mails/Mail.cs
@@ -3,29 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Mail;
 namespace CandyFramework.Common.Mails
 {
     public class Mail
     {
-        public List<string> To { get; set; }
-        public Mail(string to) : this(new List<string>() { to })
+        public MailMessage BaseMail { get; private set; }
+        public SmtpClient Smtp { get; private set; }
+        public Mail(string to, string from) : this(new List<string>() { to }, from)
         {
 
         }
-        public Mail(List<string> to)
+        public Mail(List<string> to, string from)
         {
-            To = new List<string>();
-            to.AddRange(to);
+            BaseMail = new MailMessage { From = new MailAddress(from) };
+            foreach (var item in to)
+            {
+                BaseMail.To.Add(item);
+            }
+        }
+        public void LoadSMTP(string email, string password, string host, string port, bool ssl = false)
+        {
+            int _port;
+            int.TryParse(port, out _port);
+            Smtp = new SmtpClient
+            {
+                Credentials = new System.Net.NetworkCredential(email, password),
+                Port = _port,
+                Host = host,
+                EnableSsl = ssl
+            };
         }
         public void Send()
         {
-
+            Smtp.Send(BaseMail);
         }
         public void SendAsync()
         {
             Task.Run(() =>
             {
+                Smtp.Send(BaseMail);
 
             });
         }
