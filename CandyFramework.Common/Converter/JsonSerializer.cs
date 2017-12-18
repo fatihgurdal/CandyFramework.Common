5e0955e58f07957469c4d0879e67a0d91b18620e
Fatih GÜRDAL <fatican92@gmail.com>
2017-12-18 22:21:27 +0300

    XML ve Json Serializer ayrı sınıflara çekildi

--- CandyFramework.Common/Converter/JsonSerializer.cs
+++ CandyFramework.Common/Converter/JsonSerializer.cs
@@ -1,75 +1,15 @@
 ﻿using CandyFramework.Common.Encryption;
-using CandyFramework.Common.Extentions;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace CandyFramework.Common.Converter
 {
-    public class Serialize
+    public class JsonSerializer
     {
-        #region - XML -
-
-        #region - Serialize -
-        public static void XmlSerialize<T>(T obj, string path) where T : class
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(T));
-
-            XmlWriter writer = XmlWriter.Create(path);
-
-            serializer.Serialize(writer, obj);
-
-            writer.Close();
-            ((IDisposable)writer).Dispose();
-        }
-
-        public static string XmlStringSerialize<T>(T obj) where T : class
-        {
-            XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
-
-            using (StringWriter textWriter = new StringWriter())
-            {
-                xmlSerializer.Serialize(textWriter, obj);
-                return textWriter.ToString();
-            }
-        }
-        #endregion
-
-        #region - Desrializer -
-        public static T XmlDesrializer<T>(string path) where T : class
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(T));
-
-            XmlReader reader = XmlReader.Create(path);
-
-            object obj = serializer.Deserialize(reader);
-
-            reader.Close();
-            ((IDisposable)reader).Dispose();
-
-            return (T)obj;
-        }
-
-        public static T XmlStringDesrializer<T>(string @this) where T : class
-        {
-            var reader = XmlReader.Create(@this.Trim().ToStream(), new XmlReaderSettings()
-            {
-                ConformanceLevel = ConformanceLevel.Document
-            });
-            return new XmlSerializer(typeof(T)).Deserialize(reader) as T;
-        }
-        #endregion
-
-        #endregion
-
-        #region - JSON -
-
         #region - Serialize -
         public static string JSONSerialize<T>(T serializeObject) where T : class
         {
@@ -114,8 +54,6 @@ namespace CandyFramework.Common.Converter
 
             return JsonConvert.DeserializeObject<T>(deserializeString, jsSettings);
         }
-        #endregion
-
         #endregion
     }
 }
---
+++ CandyFramework.Common/Converter/XMLSerializer.cs
@@ -0,0 +1,67 @@
+﻿using CandyFramework.Common.Extentions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CandyFramework.Common.Converter
+{
+    public class XMLSerializer
+    {
+
+        #region - Serialize -
+        public static void XmlSerialize<T>(T obj, string path) where T : class
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            XmlWriter writer = XmlWriter.Create(path);
+
+            serializer.Serialize(writer, obj);
+
+            writer.Close();
+            ((IDisposable)writer).Dispose();
+        }
+
+        public static string XmlStringSerialize<T>(T obj) where T : class
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, obj);
+                return textWriter.ToString();
+            }
+        }
+        #endregion
+
+        #region - Desrializer -
+        public static T XmlDesrializer<T>(string path) where T : class
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            XmlReader reader = XmlReader.Create(path);
+
+            object obj = serializer.Deserialize(reader);
+
+            reader.Close();
+            ((IDisposable)reader).Dispose();
+
+            return (T)obj;
+        }
+
+        public static T XmlStringDesrializer<T>(string @this) where T : class
+        {
+            var reader = XmlReader.Create(@this.Trim().ToStream(), new XmlReaderSettings()
+            {
+                ConformanceLevel = ConformanceLevel.Document
+            });
+            return new XmlSerializer(typeof(T)).Deserialize(reader) as T;
+        }
+        #endregion
+
+    }
+}
